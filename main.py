390711817ce003cbf7fa8684a10aa359a0e358cc
Dyglaz <kallkae@mail.ru>
2023-04-08 02:02:33 +0300

    Workbook4

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
+# Импортируем необходимые библиотеки
+# используем pandas и питру для обработки данных,
+# matpLotLib для визуализации и skLearn для обучения наборов данных и импорта моделей.
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
+from pandas import DataFrame,Series
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+# создадим набор данных для описания взаимосвязи между временем обучения студентов и успеваемостью
+my_dict = {'Учебное время': [0.50,0.75,1.00,1.25,1.50,1.75,1.75,2.00,2.25,2.50,2.75,3.00,3.25,3.50,4.00,4.25,4.50,4.75,5.00,5.50],
+'Оценка': [10,22,13,43,20,22,33,50,62,48,55,75,62,73,81,76,64,82,90,93]}
 
-# Добавим шума в данные, сделанные по функции f(x,b) с коэффициентами b = (0.25, 0.75, 0.5)
-beta = (0.25, 0.75, 0.5)
+dataset = pd.DataFrame(my_dict)
+print(dataset.head())
 
+# Исследуем набор данных
+print(dataset.shape)
+print(dataset.describe())
 
-def f(x, b0, b1, b2):
-    return b0 + b1 * np.exp(-b2 * x ** 2)
-
-
-# зададим массив точек хi
-xdata = np.linspace(0, 5, 50)
-
-# создаем теоретически правильные значения точек yi (без шума)
-y = f(xdata, *beta)
-
-# зашумляем эти данные
-ydata = y + 0.05 * np.random.randn(len(xdata))
-
-# Используем функцию для получения решения в виде коэффициентов функции f(x) для указанных xdata и ydata
-beta_opt, beta_cov = curve_fit(f, xdata, ydata)[0]
-print(beta_opt)
-
-# Вычислим линейное отклонение
-lin_dev = sum(beta_cov[0])
-print(lin_dev)
-
-# Вычислим квадратичное отклонение
-residuals = ydata - f(xdata, *beta_opt)
-fres = sum(residuals ** 2)
-print(fres)
-
-fig, ax = plt.subplots()
-ax.scatter(xdata, ydata)
-ax.plot(xdata, y, 'r', lw=2)
-ax.plot(xdata, f(xdata, *beta_opt), 'b', lw=2)
-ax.set_xlim(0, 5)
-ax.set_xlabel(r"Sx$", fontsize=18)
-ax.set_ylabel(r"$f(x, \beta)$", fontsize=18)
+# Нарисуем точечную диаграмму
+plt.scatter(dataset['Учебное время'], dataset['Оценка'], color = 'b', label = "данные экзамена")
+plt.xlabel("Часы")
+plt.ylabel("Оценка")
 plt.show()
+
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+print(X)
+print(y)
+# Теперь, когда у нас есть атрибуты и метки, необходимо разделить их на а обучающий и тестовый наборы.
+# Приведенный фрагмент разделяет 80% данных на обучающий набор, а 20% данных - на набор тестов
+X_train, X_test, y_train, y_test = train_test_split(X, у, test_size = 0.2, random_state = 0)
+# далее можно обучить алгоритм линейной регрессии
+# необходимо импортировать класс LinearRegression, создать его экземпляр и вызвать метод fit()
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+# приведем получившиеся коэффициенты для линии регрессии
+print(regressor.intercept_)
+print(regressor.coef_)
