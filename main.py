7dfa628b4506ff4e0f85db5ec58d34991381569a
Dyglaz <kallkae@mail.ru>
2023-04-07 14:27:19 +0300

    Workbook4

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+# Добавим шума в данные, сделанные по функции f(x,b) с коэффициентами b = (0.25, 0.75, 0.5)
+beta = (0.25, 0.75, 0.5)
 
 
-a = pd.Series([2, 4, 3, -10])
-b = pd.Series([-2, 1, 6, 1])
-print(a, b, sep='\n')
-print("Distance:", sum((a - b)**2)**0.5)
+def f(x, b0, b1, b2):
+    return b0 + b1 * np.exp(-b2 * x ** 2)
+
+
+# зададим массив точек хi
+xdata = np.linspace(0, 5, 50)
+
+# создаем теоретически правильные значения точек yi (без шума)
+y = f(xdata, *beta)
+
+# зашумляем эти данные
+ydata = y + 0.05 * np.random.randn(len(xdata))
+
+# Используем функцию для получения решения в виде коэффициентов функции f(x) для указанных xdata и ydata
+beta_opt, beta_cov = curve_fit(f, xdata, ydata)[0]
+print(beta_opt)
+
+# Вычислим линейное отклонение
+lin_dev = sum(beta_cov[0])
+print(lin_dev)
+
+# Вычислим квадратичное отклонение
+residuals = ydata - f(xdata, *beta_opt)
+fres = sum(residuals ** 2)
+print(fres)
+
+fig, ax = plt.subplots()
+ax.scatter(xdata, ydata)
+ax.plot(xdata, y, 'r', lw=2)
+ax.plot(xdata, f(xdata, *beta_opt), 'b', lw=2)
+ax.set_xlim(0, 5)
+ax.set_xlabel(r"Sx$", fontsize=18)
+ax.set_ylabel(r"$f(x, \beta)$", fontsize=18)
+plt.show()
