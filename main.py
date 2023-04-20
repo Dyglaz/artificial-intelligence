3167871e7da483cb2b87cc984b69fe3f80a75701
Dyglaz <kallkae@mail.ru>
2023-04-21 01:22:01 +0300

    Workbook_5

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
-# Импортируем необходимые библиотеки
-# используем pandas и питру для обработки данных,
-# matpLotLib для визуализации и skLearn для обучения наборов данных и импорта моделей.
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from pandas import DataFrame,Series
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-# создадим набор данных для описания взаимосвязи между временем обучения студентов и успеваемостью
-my_dict = {'Учебное время': [0.50,0.75,1.00,1.25,1.50,1.75,1.75,2.00,2.25,2.50,2.75,3.00,3.25,3.50,4.00,4.25,4.50,4.75,5.00,5.50],
-'Оценка': [10,22,13,43,20,22,33,50,62,48,55,75,62,73,81,76,64,82,90,93]}
+class Tree:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
 
-dataset = pd.DataFrame(my_dict)
-print(dataset.head())
+    def push(self, data):
+        if data < self.data:
+            if self.left:
+                self.left.push(data)
+            else:
+                self.left = Tree(data)
+        else:
+            if data == self.data:
+                return
+            if self.right:
+                self.right.push(data)
+            else:
+                self.right = Tree(data)
 
-# Исследуем набор данных
-print(dataset.shape)
-print(dataset.describe())
-
-# Нарисуем точечную диаграмму
-plt.scatter(dataset['Учебное время'], dataset['Оценка'], color = 'b', label = "данные экзамена")
-plt.xlabel("Часы")
-plt.ylabel("Оценка")
-plt.show()
+    def PrintTree(self, n=0):
+        if self.right:
+            self.right.PrintTree(n + 5)
+        print(' ' * n + str(self.data))
+        if self.left:
+            self.left.PrintTree(n + 5)
 
 
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
-print(X)
-print(y)
-# Теперь, когда у нас есть атрибуты и метки, необходимо разделить их на а обучающий и тестовый наборы.
-# Приведенный фрагмент разделяет 80% данных на обучающий набор, а 20% данных - на набор тестов
-X_train, X_test, y_train, y_test = train_test_split(X, у, test_size = 0.2, random_state = 0)
-# далее можно обучить алгоритм линейной регрессии
-# необходимо импортировать класс LinearRegression, создать его экземпляр и вызвать метод fit()
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
-# приведем получившиеся коэффициенты для линии регрессии
-print(regressor.intercept_)
-print(regressor.coef_)
+T = Tree(10)
+T.push(7)
+T.push(11)
+T.push(10)
+T.push(9)
+T.push(12)
+T.PrintTree()
